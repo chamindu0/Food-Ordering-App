a64470f3910b375b0587bbc6df2293c2df0b83e3
chamindu0 <igcysenarathna@gmail.com>
2024-07-31 23:35:39 +0530

    hi

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/useCart';
+import classes from './header.module.css';
+import { useAuth } from '../../hooks/useAuth';
+
+export default function Header() {
+  const { user, logout } = useAuth();
+
+  const { cart } = useCart();
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <Link to="/" className={classes.logo}>
+         <a href="https://fontmeme.com/sinhala/"><img src="https://fontmeme.com/permalink/240718/449f20bb8e40e8b24ad6662c5c1afc51.png" alt="sinhala" border="0"/></a> <a href="https://fontmeme.com/comic-fonts/"><img src="https://fontmeme.com/permalink/240718/ec68e0c7bd706468b0c46a6ca9748b5e.png" alt="comic-fonts" border="0"/></a>
+        </Link>
+        <nav>
+          <ul>
+            {user ? (
+              <li className={classes.menu_container}>
+                <Link to="/dashboard">{user.name}</Link>
+                <div className={classes.menu}>
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/orders">Orders</Link>
+                  <a onClick={logout}>Logout</a>
+                </div>
+              </li>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
+
+            <li>
+              <Link to="/cart">
+                Cart
+                {cart.totalCount > 0 && (
+                  <span className={classes.cart_count}>{cart.totalCount}</span>
+                )}
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
